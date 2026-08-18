30e0bb94305832906eef52e6c2d81f9db170c7eb
trishamadhusudhan <trisha30339@gmail.com>
2026-08-19 00:08:32 +0530

    Component no.3

--- day/day12/my-react-app/src/App.jsx
+++ day/day12/my-react-app/src/App.jsx
@@ -1,10 +1,12 @@
 import { Welcome } from "./components/P1.jsx";
 import Greet from "./components/P2.jsx";
+import { MyMovies } from "./components/P3.jsx";
 function App() {
   return (
     <div>
       <Welcome />
       <Greet />
+      <MyMovies />
     </div>   
   );
 }
---
+++ day/day12/my-react-app/src/components/P3.jsx
@@ -0,0 +1,10 @@
+export function MyMovies() {
+    return (
+        <>
+        <ul>
+            <li>The Odyssey</li>
+            <li>Frozen</li>
+            <li>Kinds of Kindness</li>
+        </ul></>
+    );
+}
