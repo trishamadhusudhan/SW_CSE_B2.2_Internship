3c7af82f296a5fc9be603d384a1fec82fa21e523
trishamadhusudhan <trisha30339@gmail.com>
2026-08-18 23:34:33 +0530

    Function function | export default

--- day/day12/my-react-app/src/App.jsx
+++ day/day12/my-react-app/src/App.jsx
@@ -1,8 +1,10 @@
-import { Wecome } from "./components/P1.jsx";
+import { Welcome } from "./components/P1.jsx";
+import Greet from "./components/P2.jsx";
 function App() {
   return (
     <div>
       <Welcome />
+      <Greet />
     </div>   
   );
 }
---
+++ day/day12/my-react-app/src/components/P2.jsx
@@ -0,0 +1,8 @@
+export default function greet() {
+    return(
+        <>
+            <h6>Hello my name is Trisha</h6>
+            <p>I'm learning React JS now.</p>
+        </>
+    )
+}
