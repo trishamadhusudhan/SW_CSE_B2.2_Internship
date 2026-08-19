67e34b5c35fbdf531334f87833223ea4f6005f3a
trishamadhusudhan <trisha30339@gmail.com>
2026-08-19 11:12:48 +0530

    default value for prop

--- day/day13/day-13-react-app/src/App.jsx
+++ day/day13/day-13-react-app/src/App.jsx
@@ -1,5 +1,6 @@
 import { Welcome } from "./components/P1";
 import { ProductCard } from "./components/P2";
+import headphones from "./assets/sony.jpg";
 import './App.css'
 function App() {
   
@@ -7,9 +8,11 @@ function App() {
     <>
      <Welcome name="Trisha"/>
      <ProductCard 
-     name="Sony ULT Wear Noise Cancellation Wireless Bluetooth Over Ear Headphones with Massive Bass"
+    //  name="Sony ULT Wear Noise Cancellation Wireless Bluetooth Over Ear Headphones with Massive Bass"
      price={15999}
-     image="https://m.media-amazon.com/images/I/610DB8Cwm7L._SY450_.jpg" />
+    //  image="https://m.media-amazon.com/images/I/610DB8Cwm7L._SY450_.jpg" 
+     image={headphones} 
+     />
      
     </>
   )
--- day/day13/day-13-react-app/src/components/P2.jsx
+++ day/day13/day-13-react-app/src/components/P2.jsx
@@ -1,4 +1,4 @@
-export function ProductCard({name,price,image}) {
+export function ProductCard({name="New Product",price,image}) {
     return(
         <div className="card">
             <img src={image} alt={name} />
