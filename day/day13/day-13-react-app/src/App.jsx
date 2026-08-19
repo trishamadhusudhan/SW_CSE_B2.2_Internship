44ef4cd9931fa014a944681ef66b0e53598598f2
trishamadhusudhan <trisha30339@gmail.com>
2026-08-19 11:00:26 +0530

    passing multiple props

--- day/day13/day-13-react-app/src/App.jsx
+++ day/day13/day-13-react-app/src/App.jsx
@@ -1,10 +1,16 @@
-import { Welcome } from "./components/P1.jsx";
- function App(){
-
+import { Welcome } from "./components/P1";
+import { ProductCard } from "./components/P2";
+import './App.css'
+function App() {
   
   return (
     <>
-     <Welcome name="Trisha" />
+     <Welcome name="Trisha"/>
+     <ProductCard 
+     name="Sony ULT Wear Noise Cancellation Wireless Bluetooth Over Ear Headphones with Massive Bass"
+     price={15999}
+     image="https://m.media-amazon.com/images/I/610DB8Cwm7L._SY450_.jpg" />
+     
     </>
   )
 }
