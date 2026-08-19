200e32e7bd75ed2a6839ee6a431fe764cbd808b0
trishamadhusudhan <trisha30339@gmail.com>
2026-08-19 11:28:32 +0530

    parent component & child component in the same file

--- day/day13/day-13-react-app/src/App.jsx
+++ day/day13/day-13-react-app/src/App.jsx
@@ -1,21 +1,24 @@
 import { Welcome } from "./components/P1";
 import { ProductCard } from "./components/P2";
 import headphones from "./assets/sony.jpg";
-import './App.css'
+import ButtonDemo from "./components/P3";
+import "./App.css";
+
 function App() {
-  
   return (
     <>
-     <Welcome name="Trisha"/>
-     <ProductCard 
-    //  name="Sony ULT Wear Noise Cancellation Wireless Bluetooth Over Ear Headphones with Massive Bass"
-     price={15999}
-    //  image="https://m.media-amazon.com/images/I/610DB8Cwm7L._SY450_.jpg" 
-     image={headphones} 
-     />
-     
+      <Welcome name="Trisha" />
+
+      <ButtonDemo />
+
+      <ProductCard
+        //  name="Sony ULT Wear Noise Cancellation Wireless Bluetooth Over Ear Headphones with Massive Bass"
+        price={15999}
+        //  image="https://m.media-amazon.com/images/I/610DB8Cwm7L._SY450_.jpg"
+        image={headphones}
+      />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
