3c43237267a1b6e61ac2f5b7d9aef147991fa968
trishamadhusudhan <trisha30339@gmail.com>
2026-08-19 12:55:00 +0530

    State eg 3

--- day/day13/day-13-react-app/src/App.jsx
+++ day/day13/day-13-react-app/src/App.jsx
@@ -4,11 +4,14 @@ import headphones from "./assets/sony.jpg";
 import ButtonDemo from "./components/P3";
 import Counter from "./components/StateExampleOne";
 import ToggleText from "./components/StateExampleTwo";
+import NameInput from "./components/StateExampleThree";
 import "./App.css";
 
 function App() {
   return (
     <>
+      <NameInput/>
+
       <ToggleText />
 
       <Counter />
